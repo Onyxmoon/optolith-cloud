66ce795915c61ab8d9e955b217c4eab800b8fa89
Philipp Borucki <p.borucki@icloud.com>
2019-08-27 20:52:27 +0200

    Fix use statement in main controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends Controller
 {
